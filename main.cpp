35421edabfa84a2dc0f440c7ad99139038aead7b
Martin Turcotte <tirpidz@gmail.com>
2021-01-24 22:30:14 -0500

    fixes

--- main.cpp
+++ main.cpp
@@ -52,48 +52,48 @@
 
 #ifndef QT_NO_SYSTEMTRAYICON
 
-#include "background.h"
-#include "window.h"
 #include <QMessageBox>
+#include "window.h"
+#include "background.h"
 
-int main(int argc, char *argv[]) {
-  Q_INIT_RESOURCE(systray);
+int main(int argc, char *argv[])
+{
+    Q_INIT_RESOURCE(systray);
 
-  QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+    QApplication app(argc, argv);
 
-  QApplication app(argc, argv);
+    if (!QSystemTrayIcon::isSystemTrayAvailable()) {
+        QMessageBox::critical(nullptr, QObject::tr("Systray"),
+                              QObject::tr("I couldn't detect any system tray "
+                                          "on this system."));
+        return 1;
+    }
+    QApplication::setQuitOnLastWindowClosed(false);
 
-  if (!QSystemTrayIcon::isSystemTrayAvailable()) {
-    QMessageBox::critical(nullptr, QObject::tr("Systray"),
-                          QObject::tr("I couldn't detect any system tray "
-                                      "on this system."));
-    return 1;
-  }
-  QApplication::setQuitOnLastWindowClosed(false);
-
-  background back;
-  back.start();
-  Window window;
-  window.show();
-  return app.exec();
+    background b;
+    b.start();
+    Window window;
+    window.show();
+    return app.exec();
 }
 
 #else
 
-#include <QDebug>
 #include <QLabel>
+#include <QDebug>
 
-int main(int argc, char *argv[]) {
-  QApplication app(argc, argv);
-  QString text("QSystemTrayIcon is not supported on this platform");
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QString text("QSystemTrayIcon is not supported on this platform");
 
-  QLabel *label = new QLabel(text);
-  label->setWordWrap(true);
+    QLabel *label = new QLabel(text);
+    label->setWordWrap(true);
 
-  label->show();
-  qDebug() << text;
+    label->show();
+    qDebug() << text;
 
-  app.exec();
+    app.exec();
 }
 
 #endif
